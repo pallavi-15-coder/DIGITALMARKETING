220c99a4c28d43f6ca58189bbf69b559c088feb8
pallavi-15-coder <pallavidoiphode11@gmail.com>
2026-08-28 13:21:05 +0530

    fix About Us image

--- src/Component/AboutUs.jsx
+++ src/Component/AboutUs.jsx
@@ -1,46 +1,39 @@
 import React from 'react'
 import AboutUsImage from '../assets/AboutUs.jpg'
+
 const AboutUs = () => {
   return (
-  
     <div className='items-center p-5'>
-       
-            <div className='ml-200'>
-              <h1 className='text-4xl font-bold text-gray-500  text-black-900'>
-                ABOUT US
-              </h1>
-              </div>
 
-              
-              <div className=" absolute -ml-10  mb-20">
-             <img
-              src={AboutUs} alt="About Us" className=" w-140 h-90 ml-10  rounded-2xl shadow-3xl"
-             />
-             </div>
+      <div className='ml-200'>
+        <h1 className='text-4xl font-bold text-gray-500 text-black-900'>
+          ABOUT US
+        </h1>
+      </div>
 
-             <div className='ml-180 p-8'>
-                <h1 className='font-normal text-xl flex justify-end w-90'>
-                  We are a results-driven digital marketing company helping businesses grow online,
-                   reach the right audience, and achieve measurable results.
-                </h1>
-                 
-                <h1 className='font-normal text-xl p-2 w-90'>
-                    Our expertise in SEO, Google Ads, Social Media, Content Marketing, and
-                     Web Development helps brands build a strong digital presence and achieve sustainable growth.
-                </h1>
+      <div className="absolute -ml-10 mb-20">
+        <img
+          src={AboutUsImage}
+          alt="About Us"
+          className="w-140 h-90 ml-10 rounded-2xl shadow-3xl"
+        />
+      </div>
 
-                 </div>
-             
-                
-              </div>  
-              
-    
-   
+      <div className='ml-180 p-8'>
+        <h1 className='font-normal text-xl flex justify-end w-90'>
+          We are a results-driven digital marketing company helping businesses grow online,
+          reach the right audience, and achieve measurable results.
+        </h1>
+
+        <h1 className='font-normal text-xl p-2 w-90'>
+          Our expertise in SEO, Google Ads, Social Media, Content Marketing, and
+          Web Development helps brands build a strong digital presence and achieve sustainable growth.
+        </h1>
+      </div>
+
+    </div>
   )
 }
 
-export default AboutUs;
-
-
-
+export default AboutUs
 
